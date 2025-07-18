08a36e96e2f18f64d80232005a72d9bff0be017d
Dioneibi-rip <fandubdreams@gmail.com>
2025-07-18 12:52:30 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -116,18 +116,6 @@ let tiktok = 'https://tiktok.com'
 global.redes = [canal, canal2, git, youtube, github, facebook, tiktok].getRandom()
 
 
-const iconos = [ 
-  'https://qu.ax/RsvOR.jpg',
-  'https://qu.ax/PtdyH.jpg',
-  'https://qu.ax/UvXvG.jpg',
-  'https://qu.ax/govcr.jpg',
-  'https://qu.ax/oVdPe.jpg',
-  'https://qu.ax/SXPJd.jpg',
-]
-
-global.icono = iconos[Math.floor(Math.random() * iconos.length)]
-
-
 // ──────────────────────────────────────────────╮
 // Ruta del archivo actual
 // ──────────────────────────────────────────────╯
