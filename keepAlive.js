838eb4c9a65edcc08e7994ac9721abebf4f93da8
Dioneibi-rip <fandubdreams@gmail.com>
2025-07-20 11:56:09 -0400

    Delete keepAlive.js

--- keepAlive.js
+++
@@ -1,6 +0,0 @@
-// keepAlive.js
-import express from 'express';
-const app = express();
-app.get('/', (req, res) => res.send('Bot activo.'));
-app.listen(3000, () => console.log('Servidor Express en http://localhost:3000'));
-export default app;
