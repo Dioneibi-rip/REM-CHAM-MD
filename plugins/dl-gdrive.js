7760915a9c469228b2d41e312114efff9b2aa09f
Dioneibi-rip <fandubdreams@gmail.com>
2025-07-23 17:28:40 -0400

    Actualizar dl-gdrive.js

--- plugins/dl-gdrive.js
+++ plugins/dl-gdrive.js
@@ -1,29 +1,45 @@
+import axios from 'axios';
 
-import fg from 'api-dylux' 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-	if (!args[0]) throw `✳️ Ingresa un link de Google Drive\n\n✔️ Ejemplo :\n*${usedPrefix + command}* https://drive.google.com/file`
+  if (!args[0]) throw `✳️ Ingresa un link de Google Drive\n\n✔️ Ejemplo :\n*${usedPrefix + command}* https://drive.google.com/file`;
 
-	m.react(rwait) 
-	try {
-	let res = await fg.GDriveDl(args[0])
-	 await m.reply(`
+  m.react(rwait);
+
+  try {
+    let res = await axios.get(`https://api.siputzx.my.id/api/d/gdrive?url=${encodeURIComponent(args[0])}`);
+    let result = res.data;
+
+    if (!result.status || !result.data || !result.data.download) {
+      throw new Error('No se pudo obtener el archivo');
+    }
+
+    const { name, download } = result.data;
+
+    await m.reply(`
 ≡ *Google Drive DL*
 
-▢ *Number:* ${res.fileName}
-▢ *Size:* ${res.fileSize}
-▢ *type:* ${res.mimetype}`)
-		
-	conn.sendMessage(m.chat, { document: { url: res.downloadUrl }, fileName: res.fileName, mimetype: res.mimetype }, { quoted: m })
-	m.react(done)
-   } catch {
-	m.reply('Error: Checa bien el link o prueba con otro') 
-  }
-}
-handler.help = ['gdrive']
-handler.tags = ['downloader', 'premium']
-handler.command = ['gdrive']
-handler.credit = true
-handler.premium = true
+▢ *Number:* ${name}
+▢ *Size:* Desconocido
+▢ *type:* Desconocido`);
 
-export default handler
+    await conn.sendMessage(m.chat, {
+      document: { url: download },
+      fileName: name,
+      mimetype: 'application/octet-stream' // puedes cambiarlo si sabes el tipo
+    }, { quoted: m });
+
+    m.react(done);
+
+  } catch {
+    m.reply('Error: Checa bien el link o prueba con otro');
+  }
+};
+
+handler.help = ['gdrive'];
+handler.tags = ['downloader', 'premium'];
+handler.command = ['gdrive'];
+handler.credit = true;
+handler.premium = true;
+
+export default handler;
