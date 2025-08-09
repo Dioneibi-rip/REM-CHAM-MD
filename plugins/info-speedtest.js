04bb4ecb3f7e1d13185dd735e09f34f72a827678
Dioneibi-rip <fandubdreams@gmail.com>
2025-08-09 17:19:36 -0400

    Actualizar info-speedtest.js

--- plugins/info-speedtest.js
+++ plugins/info-speedtest.js
@@ -1,44 +1,34 @@
-import cp from 'child_process';
-import { promisify } from 'util';
-const exec = promisify(cp.exec).bind(cp);
-
-const fkontak = {
-    key: {
-        participant: '0@s.whatsapp.net',
-        remoteJid: 'status@broadcast'
-    },
-    message: {
-        contactMessage: {
-            displayName: this.getName ? this.getName(m.sender) : m.pushName,
-            vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;${this.getName ? this.getName(m.sender) : m.pushName};;;\nFN:${this.getName ? this.getName(m.sender) : m.pushName}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nEND:VCARD`
+const handler = async (m, { conn }) => {
+    const fkontak = {
+        key: {
+            participant: '0@s.whatsapp.net',
+            remoteJid: 'status@broadcast'
+        },
+        message: {
+            contactMessage: {
+                displayName: conn.getName ? conn.getName(m.sender) : m.pushName,
+                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;${conn.getName ? conn.getName(m.sender) : m.pushName};;;\nFN:${conn.getName ? conn.getName(m.sender) : m.pushName}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nEND:VCARD`
+            }
         }
-    }
-};
+    };
 
-const handler = async (m) => {
     let o;
     try {
-      conn.reply(m.chat, `💙 Speed Test....`, m, )
-      o = await exec('python3 ./lib/ookla-speedtest.py --secure --share');
-        const {stdout, stderr} = o;
+        conn.reply(m.chat, `💙 Speed Test....`, m)
+        o = await exec('python3 ./lib/ookla-speedtest.py --secure --share');
+        const { stdout, stderr } = o;
+
         if (stdout.trim()) {
             const match = stdout.match(/http[^"]+\.png/);
             const urlImagen = match ? match[0] : null;
-            await conn.sendMessage(m.chat, {image: {url: urlImagen}, caption: stdout.trim()}, {quoted: fkontak});
+            await conn.sendMessage(m.chat, { image: { url: urlImagen }, caption: stdout.trim() }, { quoted: fkontak });
         }
-        if (stderr.trim()) { 
+        if (stderr.trim()) {
             const match2 = stderr.match(/http[^"]+\.png/);
-            const urlImagen2 = match2 ? match2[0] : null;    
-            await conn.sendMessage(m.chat, {image: {url: urlImagen2}, caption: stderr.trim()}, {quoted: fkontak});
+            const urlImagen2 = match2 ? match2[0] : null;
+            await conn.sendMessage(m.chat, { image: { url: urlImagen2 }, caption: stderr.trim() }, { quoted: fkontak });
         }
     } catch (e) {
-        o = e.message;
-        return m.reply(o)
+        return m.reply(e.message);
     }
 };
-handler.help = ['speedtest'];
-handler.tags = ['info'];
-handler.command = ['speedtest', 'stest', 'test'];
-handler.register = true;
-
-export default handler;
