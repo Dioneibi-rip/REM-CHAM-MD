a9d2294127cf76bc49da3953c46383cd17fff7e2
Dioneibi-rip <fandubdreams@gmail.com>
2025-08-11 18:39:46 -0400

    Actualizar tool-ytcomment.js

--- plugins/tool-ytcomment.js
+++ plugins/tool-ytcomment.js
@@ -1,12 +1,22 @@
 let handler = async (m, { conn, text }) => {
-    if (!text) throw 'No Text'
-    conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/youtube-comment', {
-    avatar: await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+  if (!text) throw '🚫 ᴇsᴄʀɪʙᴇ ᴜɴ ᴄᴏᴍᴇɴᴛᴀʀɪᴏ ᴘᴀʀᴀ ǫᴜᴇ ᴀᴘᴀʀᴇᴢᴄᴀ ᴇɴ ʟᴀ ɪᴍᴀɢᴇɴ.'
+
+  let avatar = await conn.profilePictureUrl(m.sender, 'image')
+    .catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+
+  let username = await conn.getName(m.sender)
+
+  let url = global.API('https://some-random-api.com', '/canvas/misc/youtube-comment', {
+    avatar: avatar,
     comment: text,
-    username: conn.getName(m.sender)
-    }), 'error.png', '*GRACIAS POR COMENTAR*', m)
-    }
-    handler.help = ['ytcomment <comment>']
-    handler.tags = ['maker'] 
-    handler.command = /^(ytcomment)$/i
-    export default handler
+    username: username
+  })
+
+  conn.sendFile(m.chat, url, 'ytcomment.png', '*✅ ¡Gracias por tu comentario!*', m)
+}
+
+handler.help = ['ytcomment <comentario>']
+handler.tags = ['maker']
+handler.command = /^(ytcomment)$/i
+
+export default handler
