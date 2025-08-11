564f3b3c5ba7c37aaf93cd568fa1cf7568048b65
Dioneibi-rip <fandubdreams@gmail.com>
2025-08-11 01:39:30 -0400

    Add files via upload

---
+++ plugins/jadibot-reset.js
@@ -0,0 +1,21 @@
+let handler = async (m, { conn }) => {
+    let chat = global.db.data.chats[m.chat];
+
+    if (!chat || !chat.botPrimario) {
+        console.log(`[ResetBot] Intento de reseteo en ${m.chat}, pero no había bot primario.`);
+        return;
+    }
+
+    console.log(`[ResetBot] Reseteando configuración para el chat: ${m.chat}`);
+    chat.botPrimario = null;
+
+    await m.reply(`✐ ¡Listo! Se ha restablecido la configuración.\n> A partir de ahora, todos los bots responderán nuevamente en este grupo.`);
+}
+
+handler.customPrefix = /^(resetbot|resetprimario|botreset)$/i;
+handler.command = new RegExp;
+
+handler.group = true;
+handler.admin = true;
+
+export default handler;
---
+++ plugins/jadibot-setprimary.js
@@ -0,0 +1,41 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    let chat = global.db.data.chats[m.chat];
+
+    if (m.mentionedJid.length === 0) {
+        let reset = text.toLowerCase() === 'reset' || text.toLowerCase() === 'restablecer';
+        if (reset) {
+            if (!chat.botPrimario) return m.reply('《✧》 No hay ningún bot primario establecido en este grupo.');
+
+            console.log(`[Bot Primario] Reseteando configuración para el chat: ${m.chat}`);
+            chat.botPrimario = null;
+            await m.reply(`✐ Se ha restablecido la configuración. Ahora todos los bots responderán nuevamente en este grupo.`);
+            return;
+        }
+
+        return m.reply(`《✧》 Debes mencionar a un bot del grupo para establecerlo como primario.\n\n> *Ejemplo:* ${usedPrefix + command} @tagdelbot\n\n> ❀ También puedes usar *resetbot* (SIN PREFIJOS) para que todos los bots vuelvan a responder.`);
+    }
+
+    let botJid = m.mentionedJid[0];
+    chat.botPrimario = botJid;
+
+    console.log(`[Bot Primario SET] Chat: ${m.chat} | JID Guardado: ${botJid}`);
+
+    let botName = conn.getName(botJid);
+    let response = `✐ Se ha establecido a *@${botJid.split('@')[0]}* como bot primario de este grupo.
+> A partir de ahora, todos los comandos del grupo serán ejecutados por *@${botJid.split('@')[0]}*.
+
+> *Nota:* Si esto fue un error, puedes restablecer la configuración usando el comando \`resetbot\` (sin prefijo). Este es el único comando que no está restringido y será reconocido por cualquier bot (sólo de Ruby) en el grupo.`;
+
+    await conn.sendMessage(m.chat, { 
+        text: response, 
+        mentions: [botJid] 
+    }, { quoted: m });
+}
+
+handler.help = ['setbotprimario @bot', 'setbot @bot'];
+handler.tags = ['grupo'];
+handler.command = ['setprimary', 'botprimario', 'setprimarybot', 'setbot'];
+handler.group = true;
+handler.admin = true;
+
+export default handler;
