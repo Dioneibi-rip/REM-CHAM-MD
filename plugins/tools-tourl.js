52ce904b22850a642a6de4290ca1025e18cb53c2
Dioneibi-rip <fandubdreams@gmail.com>
2025-08-11 01:32:47 -0400

    Actualizar tools-tourl.js

--- plugins/tools-tourl.js
+++ plugins/tools-tourl.js
@@ -5,7 +5,7 @@ import fetch from 'node-fetch'
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) return conn.reply(m.chat, `⚠️ ᴘᴏʀ ғᴀᴠᴏʀ, ʀᴇsᴘᴏɴᴅᴀ ᴀ ᴜɴᴀ *ɪᴍᴀɢᴇɴ* ᴏ *ᴠɪᴅᴇᴏ.*`, m)
+  if (!mime) return conn.reply(m.chat, `⚠️ 𝚙𝚘𝚛 𝚏𝚊𝚟𝚘𝚛, 𝚛𝚎𝚜𝚙𝚘𝚗𝚍𝚊 𝚊 𝚞𝚗𝚊 *𝙸𝚖𝚊𝚐𝚎𝚗* 𝚘 *𝚅𝚒́𝚍𝚎𝚘*`, m)
 
   await m.react(rwait)
 
@@ -15,13 +15,13 @@ let handler = async (m) => {
     let link = await (isTele ? uploadImage : uploadFile)(media)
     let img = await (await fetch(`${link}`)).buffer()
 
-    let txt = `╭───〔 𝙴𝙽𝙻𝙰𝙲𝙴 ─ ʟɪɴᴋ 〕───╮\n`
-    txt += `│ ✦ *ᴇɴʟᴀᴄᴇ:* ${link}\n`
-    txt += `│ ✦ *ᴀᴄᴏʀᴛᴀᴅᴏ:* ${await shortUrl(link)}\n`
-    txt += `│ ✦ *ᴛᴀᴍᴀñᴏ:* ${formatBytes(media.length)}\n`
-    txt += `│ ✦ *ᴇxᴘɪʀᴀᴄɪóɴ:* ${isTele ? 'ɴᴏ ᴇxᴘɪʀᴀ' : 'ᴅᴇsᴄᴏɴᴏᴄɪᴅᴏ'}\n`
-    txt += `╰─────────────────────╯\n`
-    txt += `> 👤 ${dev}`
+    let txt = `╭───〔 ʟɪɴᴋ - ᴇɴʟᴀᴄᴇ 〕───╮\n\n`
+        txt += `✦ 𝙴𝚗𝚕𝚊𝚌𝚎 : ${link}\n`
+        txt += `✦ 𝙰𝚌𝚘𝚛𝚝𝚊𝚍𝚘 : ${await shortUrl(link)}\n`
+        txt += `✦ 𝚃𝚊𝚖𝚊𝚗̃𝚘 : ${formatBytes(media.length)}\n`
+        txt += `✦ 𝙴𝚡𝚙𝚒𝚛𝚊𝚌𝚒𝚘́𝚗 : ${isTele ? '𝙽𝚘 𝚎𝚡𝚙𝚒𝚛𝚊' : '𝙳𝚎𝚜𝚌𝚘𝚗𝚘𝚌𝚒𝚍𝚘'}\n\n`
+        txt += `╰───────────────✦\n`
+        txt += `> ʙʏ ${dev}`
 
     await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, fkontak)
     await m.react(done)
@@ -39,12 +39,10 @@ handler.command = ['tourl', 'upload']
 export default handler
 
 function formatBytes(bytes) {
-  if (bytes === 0) {
-    return '0 B'
-  }
+  if (bytes === 0) return '0 B';
   const sizes = ['B', 'KB', 'MB', 'GB', 'TB']
   const i = Math.floor(Math.log(bytes) / Math.log(1024))
-  return `${(bytes / 1024 ** i).toFixed(2)} ${sizes[i]}`
+  return `${(bytes / 1024 ** i).toFixed(2)} ${sizes[i]}`;
 }
 
 async function shortUrl(url) {
